5f437012bd7457b20801e612d108a21458149a5e
Stuart Wright <=>
2020-03-05 23:53:45 +0000

    added name of logged in user to title bar so always visible

--- frontend/src/components/account/Logout.js
+++ frontend/src/components/account/Logout.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
 import { useAuthState } from '../../contexts/AuthContext'
 
 const Logout = () => {
-  const { setState } = useAuthState()
+  const {
+    state: { user },
+    setState
+  } = useAuthState()
   const logout = () => {
     localStorage.removeItem('token')
     setState({ status: 'success', error: null, user: null })
   }
 
   return (
-    <Button onClick={logout} color="inherit">
-      Logout
-    </Button>
+    <>
+      <Typography style={{ marginRight: 25 }} variant="body2">
+        Logged in as <span style={{ fontWeight: 'bold' }}>{user.username}</span>
+      </Typography>
+      <Button onClick={logout} color="inherit" variant="outlined">
+        Logout
+      </Button>
+    </>
   )
 }
 
